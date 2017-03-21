d11e1face9c69df5eeb05853a513e3590821d67a
朱振超 <zhuzhenchao77@gmail.com>
2017-03-21 21:02:23 +0800

    canvas绘图更改样式

--- js/script.js
+++ js/script.js
@@ -4,15 +4,15 @@ let height = canvas.height
 let con = canvas.getContext('2d')
 let body = document.getElementsByTagName('body')[0]
 
-function randomNumber() {
-    return Math.floor(Math.random() * 100 + 100)
+function randomNumber(i) {
+    return Math.floor(Math.random() * 60 + 16 * i)
 }
 
 function arrMaker() {
     let pointArr = []
     for (let i = 0; i < 36; i++) {
-        let xDisten = 39 * i
-        let yDisten = randomNumber()
+        let xDisten = width - 39 * i
+        let yDisten = randomNumber(i)
         let temp = [xDisten, yDisten]
         pointArr.push(temp)
     }
@@ -26,13 +26,16 @@ function paint() {
         con.moveTo(point[j][0], point[j][1])
         con.lineTo(point[j + 1][0], point[j + 1][1])
         con.lineTo(point[j + 2][0], point[j + 2][1])
-        con.fillStyle = `hsla(${360 - 12 * j},100%,70%,.7)`
+        let color = `hsla(${360 - 10 * j},100%,70%,.7)`
+        con.fillStyle = color
+        con.strokeStyle = color
         con.fill()
+        con.stroke()
         con.closePath()
     }
 }
 let timer = setInterval(function () {
-    // paint()
+    paint()
 }, 1700)
 body.addEventListener('click', function () {
     paint()
