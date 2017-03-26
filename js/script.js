86aa54eea4a6b521ef28401c44c37bbdcb726166
朱振超 <zhuzhenchao77@gmail.com>
2017-03-26 14:04:08 +0800

    增加主方框立体hover效果

--- js/script.js
+++ js/script.js
@@ -1,66 +1,82 @@
-let canvas = document.getElementById('canvas')
-let width = canvas.width
-let height = canvas.height
-let con = canvas.getContext('2d')
-let body = document.getElementsByTagName('body')[0]
+document.addEventListener("DOMContentLoaded", function(event) {
+    let canvas = document.getElementById('canvas')
+    let width = canvas.width
+    let height = canvas.height
+    let con = canvas.getContext('2d')
+    let body = document.getElementsByTagName('body')[0]
+    let times = 50
+    let space = Math.floor(width / times)
 
 
-let times = 50
-let space = Math.floor(width / times)
-
-
-function randomNumber(i) {
-    return Math.floor(Math.random() * 60 + 15 * i)
-}
-
-function pointMaker() {
-    let pointArr = []
-    for (let i = 0; i < times + 1; i++) {
-        let xDisten = width - space * i
-        let yDisten = randomNumber(i)
-        let temp = [xDisten, yDisten]
-        pointArr.push(temp)
+    function randomNumber(i) {
+        return Math.floor(Math.random() * 60 + 15 * i)
     }
-    return pointArr
-}
-let point = pointMaker()
 
-function paint(e) {
-    let x = e.x + randomNumber(-2)
-    let y = e.y + randomNumber(-2)
-    con.clearRect(0, 0, width, height)
-    point.shift()
-    point.push([x, y])
-    for (let j = 0; j < times - 2; j++) {
-        con.beginPath()
-        con.moveTo(point[j][0], point[j][1])
-        for (let k = 1; k < 4; k++) {
-            con.lineTo(point[j + k][0], point[j + k][1])
+    function pointMaker() {
+        let pointArr = []
+        for (let i = 0; i < times + 1; i++) {
+            let xDisten = width - space * i
+            let yDisten = randomNumber(i)
+            let temp = [xDisten, yDisten]
+            pointArr.push(temp)
         }
-        let color = `hsla(${10 * j},100%,70%,${j / 50})`
-        con.fillStyle = color
-        con.strokeStyle = color
-        con.fill()
-        con.stroke()
-        con.closePath()
+        return pointArr
     }
+    let point = pointMaker()
 
-}
-// 鼠标移动监听 paint
-body.addEventListener('mousemove', paint, false)
+    function paint(e) {
+        let x = e.x + randomNumber(-2)
+        let y = e.y + randomNumber(-2)
+        con.clearRect(0, 0, width, height)
+        point.shift()
+        point.push([x, y])
+        for (let j = 0; j < times - 2; j++) {
+            con.beginPath()
+            con.moveTo(point[j][0], point[j][1])
+            for (let k = 1; k < 4; k++) {
+                con.lineTo(point[j + k][0], point[j + k][1])
+            }
+            let color = `hsla(${10 * j},100%,70%,${j / 50})`
+            con.fillStyle = color
+            con.strokeStyle = color
+            con.fill()
+            con.stroke()
+            con.closePath()
+        }
 
-// 鼠标双击取消 对paint的监听
-body.addEventListener('dblclick', function() {
-    body.removeEventListener('mousemove', paint, false)
-}, false)
-
-body.addEventListener('click', function() {
+    }
+    // 移动监听 paint
     body.addEventListener('mousemove', paint, false)
-}, false)
 
-document.addEventListener('visibilitychange', function() {
-    if (document.visibilityState === 'hidden') {
-        normal_title = document.title;
-        document.title = '(●—●)';
-    } else document.title = normal_title;
+    // 单击取消对paint的监听
+    body.addEventListener('click', function() {
+        body.removeEventListener('mousemove', paint, false)
+    }, false)
+
+    // 双击恢复 监听paint
+    body.addEventListener('dblclick', function() {
+        body.addEventListener('mousemove', paint, false)
+    }, false)
+
+    document.addEventListener('visibilitychange', function() {
+        if (document.visibilityState === 'hidden') {
+            normal_title = document.title;
+            document.title = '(●—●)记得回来';
+        } else document.title = normal_title;
+    });
+
+
+    let container = document.getElementById('container')
+    let containerW = container.clientWidth * (-1)
+    let containerH = container.clientHeight
+    container.addEventListener('mousemove', function(e) {
+        let percentX = e.layerX / containerW
+        let percentY = e.layerY / containerH
+        this.style.transform = 'translateX(-50%) translateY(-50%) rotateX(' + percentY * 10 + 'deg) rotateY(' + percentX * 10 + 'deg)'
+    }, false)
+
+    container.addEventListener('mouseleave', function(e) {
+        this.style.transform = 'translateX(-50%) translateY(-50%)'
+    })
+
 });
